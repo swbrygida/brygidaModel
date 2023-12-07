62b2784b382516bde6fc23f89a786af5d3e38549
lukeandgladys <lukeandgladys@gmail.com>
2023-12-07 15:49:31 +0100

    fix info

--- components/stopka.vue
+++ components/stopka.vue
@@ -1,33 +1,45 @@
 <template>
-    <main>
-      <div class="stopka">
-        <p>
-          <NuxtLink  target="_blank"  to="/info"
-            >Dofinansowano ze środków Ministerstwa Kultury i Dziedzictwa Narodowego pochodzących z Funduszu Promocji Kultury - państwowego funduszu celowego</NuxtLink
-          >
-        </p>
-      </div>
-    </main>
-  </template>
-  <style>
-  @import url("https://fonts.googleapis.com/css2?family=Inknut+Antiqua:wght@300;800&display=swap");
+  <main>
+    <div class="stopka">
+      <NuxtLink  target="_blank"  to="/info">
+      <img src="../public/icons/mkidn-logo.png"  class="logotypMKiDN" alt="Logotyp Ministerstwa Kultury i
+    Dziedzictwa Narodowego"></NuxtLink>
+      <p>
+        <NuxtLink  target="_blank"  to="/info"
+          >Dofinansowano ze środków Funduszu Promocji Kultury</NuxtLink
+        >
+      </p>
+    </div>
+  </main>
+</template>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Inknut+Antiqua:wght@300;800&display=swap");
 
-  .stopka {
-    width: 90vw;
-    height: auto;
-    padding: 0 5vw;
-    border-top: 2px solid #f2f2f2;
-    background-color:transparent;
-    text-align: center;
-    font-size: normal;
-    font-weight: 800;
-    position: fixed;
-    bottom: 0; left: 0;
-    font-family: "Inknut Antiqua", serif;
-    line-height: 1.8rem
-    
+.stopka {
+  width: 90vw;
+  height: auto;
+  padding: 0 5vw;
+  border-top: 2px solid #f2f2f2;
+  background-color: #fff;
+  text-align: center;
+  font-size: normal;
+  font-weight: 800;
+  position: fixed;
+  bottom: 0; left: 0;
+  font-family: "Inknut Antiqua", serif;
+  line-height: 1.8rem;
+  display: flex;
+  flex-wrap: wrap;
+  
+  align-items: center;
+  justify-content: center;
+}
 
-  }
-  
-  </style>
-  
+
+img.logotypMKiDN {
+  height: 4em!important;
+  width: auto;
+  margin: 0 2em;
+}
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: "Model Kościoła Rektoralnego w Lublinie",
+      htmlAttrs: {
+        lang: 'pl'
+      }
+    }
+  }
 })
